81962b7bfe9b0c1733c8e6b4702b8efe8f619987
Meemaw <ematej.snuderl@gmail.com>
2024-01-05 21:28:44 +0100

    work

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  const persona = "x";
+  const locale = 'en'
+  const rewritten = `/${locale}/${persona}${request.nextUrl.pathname}${request.nextUrl.search}`;
+  return NextResponse.rewrite(new URL(rewritten, request.url));
+}
+
+export const config = {
+  matcher: ["/((?!_next|.*\\..*|api|__api).*)"],
+};
+	
