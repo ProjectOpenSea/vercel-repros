f9512fdf7cea5e1a00a644ce042866d7d76060ed
Meemaw <ematej.snuderl@gmail.com>
2024-01-05 21:29:14 +0100

    work

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,8 @@ import { NextResponse, type NextRequest } from "next/server";
 export default function middleware(request: NextRequest) {
   const persona = "x";
   const locale = 'en'
-  const rewritten = `/${locale}/${persona}${request.nextUrl.pathname}${request.nextUrl.search}`;
-  return NextResponse.rewrite(new URL(rewritten, request.url));
+  request.nextUrl.pathname = `/${locale}${persona}${request.nextUrl.pathname}`
+  return NextResponse.rewrite(request.nextUrl);
 }
 
 export const config = {
